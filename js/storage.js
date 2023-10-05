13e22da312311b40caca5c3ce8aa96f255f64932
Sascha Nemeth <sascha.nemeth@me.com>
2023-10-05 05:55:41 +0200

    change structure

---
+++ js/storage.js
@@ -0,0 +1,18 @@
+const STORAGE_TOKEN = 'XNC8MLX5LB4ISE8IDUYSCJ9SQJQR8YQIUEWHY83J';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+async function setItem(key, value) {
+    const payload = {key, value, token: STORAGE_TOKEN};
+    return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)})
+        .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json()).then(res => {
+        // Verbesserter code
+        if (res.data) {
+            return res.data.value;
+        } throw `Could not find data with key "${key}".`;
+    });
+}
