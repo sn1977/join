9363a11c25eebc488143ff95a06885eef1134bd3
Sascha Nemeth <sascha.nemeth@me.com>
2023-10-07 13:49:59 +0200

    index.html position change

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+/* Js Code für Log in page */
+
+// Nach Abschluss der Animation den .frame156 sichtbar machen
+function showLogIn() {
+    document.querySelector('.img-size-login').addEventListener('animationend', function () {
+        document.querySelector('.frame156').style.visibility = 'visible';
+    });
+}
