283de965a8e4287c895a50d9a5af80fdbae51863
Sascha Nemeth <sascha.nemeth@me.com>
2023-10-08 14:03:50 +0200

    add login side second part

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 /* Js Code für Log in page */
 
-// Nach Abschluss der Animation den .frame156 sichtbar machen
+// Nach Abschluss der Animation den frame156 & frame153 sichtbar machen
 function showLogIn() {
     document.querySelector('.img-size-login').addEventListener('animationend', function () {
         document.querySelector('.frame156').style.visibility = 'visible';
+        document.querySelector('.frame153').style.visibility = 'visible';
     });
 }
+
+
