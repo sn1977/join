04888e8321f85cf07a4d1a7de4b39a7c71eb48a2
Patrick Klein <patrickklein611@gmail.com>
2023-11-07 23:16:29 +0100

    Revert "Merge branch 'main' of https://github.com/sn1977/join"
    This reverts commit 50447d0faf1b15ab47e913e626f7018a9b5d713a, reversing
    changes made to 87cc835676730136cfd380345b57cadc4a7321a0.

--- js/contacts.js
+++ js/contacts.js
@@ -1,17 +1,14 @@
-let contacts = [
-    {nameOfContact: 'Anton Mayer', emailOfContact: 'anton@gmail.com', telOfContact: '123456'},
-    {nameOfContact: 'Alfred Müller', emailOfContact: 'alfred@gmail.com', telOfContact: '789456'},
-    {nameOfContact: 'Beate Müller', emailOfContact: 'beate@gmail.com', telOfContact: '456951'},
-]
+let nameOfContact = ['Anton Mayer', 'Alfred Müller', 'Beate Müller'];
+let emailOfContact = ['anton@gmail.com', 'alfred@gmail.com', 'beate@gmail.com'];
+let telOfContact = [123456, 789456, 456951];
 
 const colors = ['#FF7A00', '#9327FF', '#6E52FF', '#FC71FF', '#FFBB2B', '#1FD7C1', '#462F8A', '#0038FF'];
+
 
 let contactNameElem;
 let contactEmailElem;
 let contactPhoneElem;
 let currentSelectedIndex = null;
-let isContactDetailsShown = false;
-
 
 function initializeContactElements() {
     contactNameElem = document.getElementById('contactName');
@@ -25,22 +22,21 @@ async function initContact() {
 }
 
 async function loadContacts() {
-    contacts = JSON.parse(await getItem('contacts')) || contacts;
-}
-
-function sortContactsByName() {
-    contacts.sort((a, b) => a.nameOfContact.localeCompare(b.nameOfContact));
+    nameOfContact = JSON.parse(await getItem('nameOfContact')) || ['Anton Mayer', 'Alfred Müller', 'Beate Müller'];
+    emailOfContact = JSON.parse(await getItem('emailOfContact')) || ['anton@gmail.com', 'alfred@gmail.com', 'beate@gmail.com'];
+    telOfContact = JSON.parse(await getItem('telOfContact')) || [123456, 789456, 456951];
 }
 
 async function newContact() {
     initializeContactElements();
 
-    contacts.push({
-        nameOfContact: contactNameElem.value,
-        emailOfContact: contactEmailElem.value,
-        telOfContact: contactPhoneElem.value,
-    });
-    await setItem('contacts', JSON.stringify(contacts));
+    nameOfContact.push(contactNameElem.value);
+    emailOfContact.push(contactEmailElem.value);
+    telOfContact.push(contactPhoneElem.value);
+
+    await setItem('nameOfContact', JSON.stringify(nameOfContact));
+    await setItem('emailOfContact', JSON.stringify(emailOfContact));
+    await setItem('telOfContact', JSON.stringify(telOfContact));
 
     resetContactField();
     closeOverlayAddContact();
@@ -54,24 +50,25 @@ function resetContactField() {
     contactPhoneElem.value = '';
 }
 
-function groupContactsByFirstLetter() {
-    sortContactsByName();
-    return contacts.reduce((groups, contact) => {
-        const firstLetter = contact.nameOfContact[0].toUpperCase();
-        if (!groups[firstLetter]) {
-            groups[firstLetter] = [];
+function groupByFirstLetter(names) {
+    return names.reduce((groups, name) => {
+        if (name && name.length > 0) {  // Überprüfen, ob der Name existiert und nicht leer ist
+            const firstLetter = name[0].toUpperCase();
+            if (!groups[firstLetter]) {
+                groups[firstLetter] = [];
+            }
+            groups[firstLetter].push(name);
         }
-        groups[firstLetter].push(contact);
         return groups;
     }, {});
 }
+
 
 function renderContacts() {
     let allContacts = document.getElementById('allContacts');
     allContacts.innerHTML = '';
 
-    // const groupedContacts = groupByFirstLetter(nameOfContact);
-    const groupedContacts = groupContactsByFirstLetter();
+    const groupedContacts = groupByFirstLetter(nameOfContact);
 
     for (const firstLetter in groupedContacts) {
         allContacts.innerHTML += `
@@ -79,18 +76,14 @@ function renderContacts() {
                 <span>${firstLetter}</span>
             </div>
             <div class="frame119">
-                <svg class="parting-line" fill="none" height="2" viewBox="0 0 354 2" width="354" xmlns="http://www.w3.org/2000/svg">
+                <svg fill="none" height="2" viewBox="0 0 354 2" width="354" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 1H353" stroke="#D1D1D1" stroke-linecap="round"/>
                 </svg>
             </div>`;
 
-        // groupedContacts[firstLetter].forEach((name) => {
-        //     const originalIndex = nameOfContact.indexOf(name);
-        //     allContacts.innerHTML += showContact(name, originalIndex);
-        // });
-        groupedContacts[firstLetter].forEach((contact) => {
-            const index = contacts.indexOf(contact);
-            allContacts.innerHTML += showContact(contact.nameOfContact, index);
+        groupedContacts[firstLetter].forEach((name) => {
+            const originalIndex = nameOfContact.indexOf(name);
+            allContacts.innerHTML += showContact(name, originalIndex);
         });
     }
 }
@@ -113,34 +106,17 @@ function getColorByIndex(index) {
 }
 
 
-// function showContact(name, index) {
-//     const initials = getInitials(name);
-//     const color = getColorByIndex(index);
-//
-//     return `
-//         <div class="contact-name contact-hover" id="contactNameBox${index}" onclick="showContactDetails(${index})">
-//             <div class="profile-badge contact-hover" id="profileBadge${index}">
-//                 <div class="group9" style="background-color: ${color};">${initials}</div> <!-- Verwendung der Initialen -->
-//                 <div class="frame81">
-//                     <span class="contactName">${name}</span>
-//                     <span class="contactEmail">${emailOfContact[index]}</span>
-//                 </div>
-//             </div>
-//         </div>`;
-// }
-
 function showContact(name, index) {
-    const contact = contacts[index];
-    const initials = getInitials(contact.nameOfContact);
+    const initials = getInitials(name);
     const color = getColorByIndex(index);
 
     return `
         <div class="contact-name contact-hover" id="contactNameBox${index}" onclick="showContactDetails(${index})">
-            <div class="profile-badge contact-hover" id="profileBadge${index}" >
+            <div class="profile-badge contact-hover" id="profileBadge${index}">
                 <div class="group9" style="background-color: ${color};">${initials}</div> <!-- Verwendung der Initialen -->
                 <div class="frame81">
-                    <span class="contactName">${contact.nameOfContact}</span>
-                    <span class="contactEmail">${contact.emailOfContact}</span>
+                    <span class="contactName">${name}</span>
+                    <span class="contactEmail">${emailOfContact[index]}</span>
                 </div>
             </div>
         </div>`;
@@ -173,11 +149,6 @@ function showContactDetails(i) {
     currentSelectedIndex = i;
     displayWindows(i);
     displayFloatingContactDetails(i);
-
-    isContactDetailsShown = true;
-
-    // Manuelles Auslösen eines resize Events
-    window.dispatchEvent(new Event('resize'));
 }
 
 
@@ -197,9 +168,8 @@ function displayWindows() {
     let contactsHeader = document.getElementById('frame40');
     let floatingContact = document.getElementById('floatingContact');
     let allRenderedContacts = document.getElementById('frame97');
-    let arrowLeft = document.getElementById('arrowLeft');
 
-    if (window.innerWidth <= 990) {
+    if (window.innerWidth <= 800) {
         if (contactsHeader) {
             applyStyles(contactsHeader, {
                 display: 'inline-flex',
@@ -218,24 +188,18 @@ function displayWindows() {
                 gap: '21px',
                 width: '650px',
                 height: '364px',
-                top: '330px',
+                top: '250px',
                 position: 'absolute',
                 left: '50px'
             });
         }
         if (allRenderedContacts) {
-            if (isContactDetailsShown) {
-                applyStyles(allRenderedContacts, {
-                    display: 'none'
-                });
-                applyStyles(arrowLeft, {
-                    display: 'block'
-                });
-            } else {
-                applyStyles(allRenderedContacts, {
-                    display: 'inline-flex',
-                });
-            }
+            applyStyles(allRenderedContacts, {
+                display: 'none',
+                // position: 'absolute',
+                // top: '120px',
+                // right: '60px'
+            });
         }
 
         // back.addEventListener('click', () => {
@@ -246,8 +210,8 @@ function displayWindows() {
         if (contactsHeader) resetStyles(contactsHeader);
         if (floatingContact) resetStyles(floatingContact);
         if (allRenderedContacts) resetStyles(allRenderedContacts);
-        if (arrowLeft) resetStyles(arrowLeft);
     }
+
 }
 
 function overlayAddContact() {
@@ -278,28 +242,16 @@ function overlayContactCreated() {
     addOverlayCreatedContact();
 }
 
-// async function deleteContact(index) {
-//     if (index >= 0 && index < nameOfContact.length) {
-//         removeContactFromArray(nameOfContact[index])
-//         // Lokale Arrays aktualisieren
-//         nameOfContact.splice(index, 1);
-//         emailOfContact.splice(index, 1);
-//         telOfContact.splice(index, 1);
-//
-//         // Die Arrays zurück in den Remote-Speicher speichern
-//         await pushBackArrays();
-//         renderContacts();
-//         hideFloatingContact();
-//     }
-// }
-
 async function deleteContact(index) {
-    if (index >= 0 && index < contacts.length) {
-        // Kontakt aus dem Array entfernen
-        contacts.splice(index, 1);
+    if (index >= 0 && index < nameOfContact.length) {
+        removeContactFromArray(nameOfContact[index])
+        // Lokale Arrays aktualisieren
+        nameOfContact.splice(index, 1);
+        emailOfContact.splice(index, 1);
+        telOfContact.splice(index, 1);
 
-        // Die Änderungen zurück in den Remote-Speicher speichern
-        await setItem('contacts', JSON.stringify(contacts));
+        // Die Arrays zurück in den Remote-Speicher speichern
+        await pushBackArrays();
         renderContacts();
         hideFloatingContact();
     }
